374ffa827de017db702008fad5e515ba359afc1e
Rafael Escribano <r.escribano1@gmail.com>
2021-04-09 13:19:44 -0400

    refactor: Update the mapp array function to simplify object creation
    Simplified the map array function so that each object is made as simply as possible.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,25 +1,58 @@
-import imageObjectArray from "./components/data";
-//TODO: Figure out how to loop through image object array and tie the generation of a new figure to the click event of the timeline
+import imageObjectArray from "../js/components/data.js";
+
+// Map through image array and create a new array
+const figureContent = imageObjectArray.map((image) => {
+    // Image should now hold single Image object
+
+    // Create a new figure
+    let tributeFigure = document.createElement("figure");
+    tributeFigure.classList.add("tribute__figure");
+    // Set figure ID to the image.id property
+    tributeFigure.setAttribute("id", image.id);
+
+    //Create a img element to populate the figure
+    let newImg = document.createElement("img");
+    newImg.setAttribute("src", image.src);
+    newImg.setAttribute("alt", image.alt);
+    // Create figcaption element to populate the figure
+    let newDesc = document.createElement("figcaption");
+    newDesc.innerText = image.alt;
+
+    // Add created elements to the figure
+    tributeFigure.append(newImg, newDesc);
+
+    // Return the new figure to the figureContent array
+    return tributeFigure;
+
+});
+
+//TODO: Get figure location. Figure out how to add the event listener and decide which image goes with which radio button press
+// this is the reference I am using: /Users/rescribano/Downloads/javascript-essential-training-2832077-main/08_16/script.js
+
+const switchFigure = (figureContent) => {
+    // Get the header
+    const header = document.querySelector("header");
+
+    // Get all the input elements within the timeline element
+    const radio = document.querySelectorAll("input");
+};
+
 /**
  * addFigure function
- * - Receives dataObj
+ * - Receives image
  * - Creates <figure> <img> <figcaption>
  * - Returns <figure>
  */
-const replaceFigure = (dataObj) => {
+/*
+function replaceFigure() {
     let newFigure = document.createElement("figure");
     let newImg = document.createElement("img");
-    newImg.setAttribute("src", dataObj.image);
-    newImg.setAttribute("alt", "");
+    newImg.setAttribute("src", image.src);
+    newImg.setAttribute("alt", image.alt);
     let newDesc = document.createElement("figcaption");
-    newDesc.innerText = dataObj.description;
+    newDesc.innerText = image.alt;
     newFigure.append(newImg, newDesc);
     return newFigure;
-};
+}
 
-
-
-// const createImage = function () {
-//     let tributeImg = document.querySelector("#img-div");
-//
-// }
+return replaceFigure();*/
