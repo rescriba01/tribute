a89f22540e1faa7183f40cc8381c326e7e844aec
Rafael Escribano <r.escribano1@gmail.com>
2021-04-13 16:38:09 -0400

    dev: Make figure image swap with another when radio button clicked
    advanced the swapFigure function to the point where I am able to swap the figure element when the radio button is clicked with the correlating figure element based on the id of both.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,55 +1,73 @@
 import imageObjectArray from "../js/components/data.js";
 
-// Get the header
-const header = document.querySelector("header");
+//TODO: Figure out how to match the created figure from the figureContent with the appropriate radio button press.
+// reference: Downloads/javascript-essential-training-2832077-main/08_16/script.js
 
-// Get the current figure element
-const original = document.querySelector("#img-div");
+const swapFigure = () => {
 
-// Map through image array and create a new array
-const figureContent = imageObjectArray.map((image) => {
-    // Image should now hold single Image object
+    // Map through image array and create a new array
+    const figureContent = imageObjectArray.map((image) => {
+        // Image should now hold single Image object
 
-    // Create a new figure
-    let tributeFigure = document.createElement("figure");
-    tributeFigure.classList.add("tribute__figure");
-    // Set figure ID to the image.id property
-    tributeFigure.setAttribute("id", image.id);
+        // Create a new figure
+        let tributeFigure = document.createElement("figure");
+        tributeFigure.classList.add("tribute__figure");
+        // Set figure ID to the image.id property
+        tributeFigure.setAttribute("id", image.id);
 
-    //Create a img element to populate the figure
-    let newImg = document.createElement("img");
-    newImg.setAttribute("src", image.src);
-    newImg.setAttribute("alt", image.alt);
-    // Create figcaption element to populate the figure
-    let newDesc = document.createElement("figcaption");
-    newDesc.innerText = image.alt;
+        //Create a img element to populate the figure
+        let newImg = document.createElement("img");
+        newImg.setAttribute("src", image.src);
+        newImg.setAttribute("alt", image.alt);
+        // Create figcaption element to populate the figure
+        let newDesc = document.createElement("figcaption");
+        newDesc.innerText = image.alt;
 
-    // Add created elements to the figure
-    tributeFigure.append(newImg, newDesc);
+        // Add created elements to the figure
+        tributeFigure.append(newImg, newDesc);
 
-    // Return the new figure to the figureContent array
-    return tributeFigure;
+        // Return the new figure to the figureContent array
+        return tributeFigure;
 
-});
+    });
 
-//TODO: Get figure location. Figure out how to add the event listener and decide which image goes with which radio button press
-// this is the reference I am using: Downloads/javascript-essential-training-2832077-main/08_16/script.js
+    // Get header
+    const header = document.querySelector("header");
 
-const switchFigure = () => {
+
+    // Create an array of the new figures
+    const tributes = [...figureContent];
+
+
     // Get all the input elements within the timeline element as a Node List
     const radio = document.querySelectorAll("input");
-    // Create an array from the Node list using Array.from() method https://attacomsian.com/blog/javascript-convert-nodelist-to-array
-    const radioArr = Array.from(radio)
 
-    radioArr.map((input) => {
-        // each radio button should now be mapped to input
-        console.log("made it before the listener");
-        input.addEventListener("click", (event) => {
-            // Check the id of the element
-            let hook = input.getAttribute(input.id);
-            console.log(hook);
-        });
-    });
+    // Create an array from the Node list using Array.from() method https://attacomsian.com/blog/javascript-convert-nodelist-to-array
+    const radioArr = [...radio];
+
+
+    // Get original figure element
+    let originalFig = document.querySelector("#img-div");
+
+
+    // event listener for click
+    function swap() {
+        for (let i = 0; i < radioArr.length; i++) {
+            radioArr[i].addEventListener("click", function () {
+                for (let j = 0; j < tributes.length; j++) {
+                    let tributeId = tributes[j].getAttribute("id")
+                    let radioId = radio[i].getAttribute("id");
+                    let radioSliced = radioId.slice(0, 6)
+                    if (tributeId === radioSliced) {
+                        originalFig.replaceWith(tributes[j])
+                        console.log(tributes[j])
+                    }
+                }
+            });
+        }
+    }
+
+    swap();
 };
 
-switchFigure();
+swapFigure();
